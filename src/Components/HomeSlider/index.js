c335c446d153b1434991851d5f417b498848a400
jatin chawda <38835306+jatinchawda1503@users.noreply.github.com>
2021-02-23 21:34:50 +0530

    function

--- src/Components/HomeSlider/index.js
+++ src/Components/HomeSlider/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../HomeSlider/HomeSlider.css'
-import SlideItems from './SlideItems';
+//import SlideItems from './SlideItems';
 import 'owl.carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
@@ -14,70 +14,153 @@ import Product4 from '../../images/product/4.png';
 import ReactOwlCarousel from 'react-owl-carousel';
 
 
-// import dynamics from 'dynamics.js/lib/dynamics.js';
-// import classie from '../../external/js/classie.js';
+import dynamics from 'dynamics.js/lib/dynamics.js';
+import classie from '../../external/js/classie.js';
 
 
-import {CircleSlideshow} from '../../external/js/Handleslider';
-//import '../../external/js/Handleslider';
+//import {CircleSlideshow} from '../../external/js/Handleslider';
+import '../../external/js/Handleslider';
 
  //var CircleSlideshow;
 class HomeSlider extends Component {
    
    render(){
-
-    // (() => {
-    //   //  document.documentElement.className = 'js';
-    //      //CircleSlideshow(document.getElementById('slideshow'));
-    //      //window["CircleSlideshow"](document.getElementById('slideshow'));
-    // })();
-
-
-    // slide = () => {
-    //     window.CircleSlideshow(document.getElementById('slideshow'));
-    //   }
-
-    // slide()
-    // {
-    //     CircleSlideshow(document.getElementById('slideshow'));
-    // }
-
     
     return (
         <div>
-			<div id="slideshow" className="slideshow" onLoad={this.slide} >
-
-                <SlideItems 
-                title = "Macadamia Skin Oil"
-                price = {300}
-                image = {Product1}
-                url= "/product/sample1"
-                s_desc="Hydration for very dry skin"
-                />
-                <SlideItems 
-                title = "Grapeseed Skin Oil"
-                price = {400}
-                image = {Product2}
-                url= "/product/sample2"
-                s_desc="Moisture control for all skin types"
-                />
-
-                <SlideItems 
-                title = "Jojoba Skin Oil"
-                price = {200}
-                image = {Product3}
-                url= "/product/sample3"
-                s_desc="Protection for sensitive skin"
-                />
-				
-
-                <SlideItems 
-                title = "Amaranth Skin Oil"
-                price = {250}
-                image = {Product4}
-                url= "/product/sample4"
-                s_desc="Rich hydration for mature skin"
-                />
+			<div id="slideshow" className="slideshow">
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Macadamia Skin Oil <span className="slide__price">$39</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product1} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product1} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Macadamia Skin Oil</h2>
+								<p className="slide__description">Hydration for very dry skin</p>
+								<div>
+									<span className="slide__price slide__price--large">$39</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>					
+                        </div>
+				</div>
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Grapeseed Skin Oil <span className="slide__price">$19</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product2} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product2} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Grapeseed Skin Oil</h2>
+								<p className="slide__description">Moisture control for all skin types</p>
+								<div>
+									<span className="slide__price slide__price--large">$19</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Jojoba Skin Oil <span className="slide__price">$35</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product3} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product3} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Jojoba Skin Oil</h2>
+								<p className="slide__description">Protection for sensitive skin</p>
+								<div>
+									<span className="slide__price slide__price--large">$35</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Amaranth Skin Oil <span className="slide__price">$29</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product4} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product4} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Amaranth Skin Oil</h2>
+								<p className="slide__description">Rich hydration for mature skin</p>
+								<div>
+									<span className="slide__price slide__price--large">$29</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Argan Skin Oil <span className="slide__price">$59</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product2} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product4} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Argan Skin Oil</h2>
+								<p className="slide__description">Moisture for problematic &amp; dry skin</p>
+								<div>
+									<span className="slide__price slide__price--large">$59</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="slide">
+					<h2 className="slide__title slide__title--preview">Avocado Skin Oil <span className="slide__price">$39</span></h2>
+					<div className="slide__item">
+						<div className="slide__inner">
+							<img className="slide__img slide__img--small" src={Product4} alt="Some image" />
+							<button className="action action--open" aria-label="View details"><i className="fa fa-plus"></i></button>
+						</div>
+					</div>
+					<div className="slide__content">
+						<div className="slide__content-scroller">
+							<img className="slide__img slide__img--large" src={Product2} alt="Some image" />
+							<div className="slide__details">
+								<h2 className="slide__title slide__title--main">Avocado Skin Oil</h2>
+								<p className="slide__description">Deep repair for stressed skin</p>
+								<div>
+									<span className="slide__price slide__price--large">$39</span>
+									<button className="button button--buy">Add to cart</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<button className="action action--close" aria-label="Close"><i className="fa fa-close"></i></button>
 			</div>
             
